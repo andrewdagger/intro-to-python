eb0f6dba376f2258c11b32ee72c6d95b47ccddd4
andrewdagger <64667803+andrewdagger@users.noreply.github.com>
2020-05-16 09:32:53 +1000

    Add files via upload

---
+++ My_first_program_DadBot.py
@@ -0,0 +1,39 @@
+## Andrew Dagger
+## 2.05.2020
+## Title --> My First Python Program
+
+import time
+
+print("Hi, this is my first robot-human interaction")
+
+time.sleep(3)
+
+print("Let's chat for a bit, shall we? ")
+
+time.sleep(3)
+
+answer1 =input("Please type your answer here ")
+
+time.sleep(1)
+
+print("My name is DadBot, what is your name? ")
+
+time.sleep(1)
+
+answer2 =input("Please type your answer here ")
+
+print(f"Very nice to meet you, {answer2}! ")
+
+time.sleep(1)
+
+print(f"Soooo, what do you do for a living, {answer2}? I'm Dadbot, the Dad who is also a bot!")
+##answer2 not being recognised in the print command
+
+time.sleep(1)
+
+answer3 =input("Please type your answer here")
+
+time.sleep(1)
+
+print("%s sounds like a fun job, can you explain it in layman terms for me please" %answer3)
+
